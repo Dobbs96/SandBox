874b20063cbf73c9f8d94002d0351661a4384065
James Dobbs <dobbs282@gmail.com>
2021-10-30 12:53:46 -0400

    added characters to use

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -1,7 +1,71 @@
+// GLOBAL
 const generateBTN = document.querySelector("#generate");
 const copyBTN = document.querySelector("#copy");
 const textArea = document.querySelector("#password");
+// used Chars
 
+const specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const upperCasedCharacters = lowerCasedCharacters.map((char) =>
+  char.toUpperCase()
+);
+
+//myFunc
 const generatedPassword = () => {
   return "abcd1234";
 };
@@ -14,6 +78,9 @@ const writePassword = () => {
 const copyText = () => {
   textArea.select();
   textArea.setSelectionRange(0, 999);
+  if (!textArea.value) {
+    return alert("Please Generate a Password First!");
+  }
   navigator.clipboard.writeText(textArea.value);
   alert("Password copied to clipboard!");
 };
